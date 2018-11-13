cadbb7441a53d23c5bcd00312705cb6420d51ba1
snoel115 <snoel115@hotmail.com>
2018-11-13 00:23:04 -0500

    test commit

---
+++ 10-jQuery/serge.js
@@ -0,0 +1,19 @@
+console.log('js connected')
+
+//var vNB = prompt('Player One: Enter Your Name , you will be Blue');
+//var vNR = prompt('Player Two: Enter Your Name, you will be Red');
+
+$('button').click(setColor);
+
+function setColor(){
+  // for (var i = 0; i < this.length; i++) {
+  //   this[i]
+  // }
+  console.log(event);
+  // $(this).css.background = Red;
+  $(this).toggleClass('setBlue');
+
+    console.log($(this).closest("td").index());
+    console.log($(this).closest("tr").index());
+}
+this is a test ... try to commit
