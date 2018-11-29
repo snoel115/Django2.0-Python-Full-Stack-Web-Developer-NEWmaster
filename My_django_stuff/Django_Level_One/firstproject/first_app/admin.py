56f37cdcbe2d7622939d5c1b63efb2044eb01823
snoel115 <snoel115@hotmail.com>
2018-11-29 17:46:58 -0500

    create fist model and database

--- My_django_stuff/Django_Level_One/firstproject/first_app/admin.py
+++ My_django_stuff/Django_Level_One/firstproject/first_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+#snoel - register the model ===========================================
+from first_app.models import AccessRecord, Topic, Webpage
+
+admin.site.register(AccessRecord)
+admin.site.register(Topic)
+admin.site.register(Webpage)
+
+# =====================================================================
--- My_django_stuff/Django_Level_One/firstproject/first_app/models.py
+++ My_django_stuff/Django_Level_One/firstproject/first_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+#snoel - create a class ===================================================
+class Topic(models.Model):
+    top_name = models.CharField(max_length=264, unique=True)
+
+    def __str__(self):
+        return self.top_name
+
+class Webpage(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=None)
+    name = models.CharField(max_length=264, unique=True)
+    url = models.URLField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class AccessRecord(models.Model):
+    name = models.ForeignKey(Webpage, on_delete=None)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
+#===========================================================================
