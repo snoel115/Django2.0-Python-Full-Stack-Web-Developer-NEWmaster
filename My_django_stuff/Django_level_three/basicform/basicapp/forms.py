6909f61d7bfc29e51be6a86872248128bcbd1e35
Serge.Noel <snoel115@hotmail.com>
2018-12-05 21:14:56 -0500

    add comments for a better understanding later on

--- My_django_stuff/Django_level_three/basicform/basicapp/forms.py
+++ My_django_stuff/Django_level_three/basicform/basicapp/forms.py
@@ -3,24 +3,30 @@ from django import forms
 from django.core import validators
 
 # snoel individual field validator
-def checkForZ(value):
+def strMustStartWithTheLetterZ(value):
     if value[0].lower() != 'z':
         raise forms.ValidationError('The Name field must start with a Z')
 
 class FormName(forms.Form):
     # snoel - to use with a indivitual field validator
-    name = forms.CharField(validators=[checkForZ])
+    name = forms.CharField(validators=[strMustStartWithTheLetterZ])
+
+    #create a second email field to double-check ... many site request the user to type his email address twice
+    # note that this step is not necessary
     email = forms.EmailField()
-    #create a second email field to double-check
     verify_email = forms.EmailField(label='Enter your email again:')
+
     text = forms.CharField(widget=forms.Textarea)
 
     # By using Django to help us, we only need to do by importing the Django Validator
+    # this is an invisible field (nicked eye) ... but as a bot populate forms in usign the html directly, the bot will entered
+    # information in this field. We expect this to be always empty. So, if there is something, a bot pushes information
     botcatcher = forms.CharField(required=False,
                                 widget=forms.HiddenInput,
                                 validators=[validators.MaxLengthValidator(0)])
 
     #snoel - this will validate the entire form at once
+    # otherwise, you can validate each fied at the time, but this will be anoying for the end user
     def clean(self):
         all_clean_data = super().clean()
         #check if the email is valid
